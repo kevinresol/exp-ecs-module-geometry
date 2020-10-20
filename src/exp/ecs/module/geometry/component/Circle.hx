6839cfecaeadcfc958ee4d42c589a2099d969a31
Kevin Leung <kevinresol@gmail.com>
2020-10-20 23:11:40 +0800

    Init

---
+++ src/exp/ecs/module/geometry/component/Circle.hx
@@ -0,0 +1,5 @@
+package exp.ecs.module.geometry.component;
+
+class Circle implements Component {
+	public var radius:Float;
+}
---
+++ src/exp/ecs/module/geometry/component/Rectangle.hx
@@ -0,0 +1,6 @@
+package exp.ecs.module.geometry.component;
+
+class Rectangle implements Component {
+	public var width:Float;
+	public var height:Float;
+}
